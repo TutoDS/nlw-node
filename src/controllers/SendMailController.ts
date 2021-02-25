838f6a86a4bf2cedfdddd11f95654e65643f648a
Daniel Sousa <jdaniel.asousa@gmail.com>
2021-02-25 15:28:00 +0000

    Feat: get relations

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -14,88 +14,82 @@ class SendMailController {
 		const surveysRepository = getCustomRepository(SurveysRepository);
 		const repository = getCustomRepository(SurveysUsersRepository);
 
-		await usersRepository
-			.findOne({ email })
-			.then(async (user) => {
-				// User founded
+		const user = await usersRepository.findOne({ email });
 
-				await surveysRepository
-					.findOne(survey_id)
-					.then(async (survey) => {
-						// Survey founded
+		if (user) {
+			// User founded
+			const survey = await surveysRepository.findOne(survey_id);
 
-						const emailPath = resolve(
-							__dirname,
-							'..',
-							'views',
-							'emails',
-							'survey.hbs'
-						);
+			if (survey) {
+				const emailPath = resolve(
+					__dirname,
+					'..',
+					'views',
+					'emails',
+					'survey.hbs'
+				);
 
-						const variables = {
-							name: user.name,
-							title: survey.title,
-							description: survey.description,
-							route: server.emailUrl,
-							user_id: user.id,
-						};
+				const variables = {
+					name: user.name,
+					title: survey.title,
+					description: survey.description,
+					route: server.emailUrl,
+					user_id: user.id,
+				};
 
-						const foundedResults = await repository.findOne({
-							where: [{ user_id: user.id }, { value: null }],
-						});
-
-						if (foundedResults) {
-							await SendMailService.execute(
-								email,
-								survey.title,
-								variables,
-								emailPath
-							);
-
-							return res.status(200).json({
-								message: 'Survey email has send with success',
-								code: 200,
-								info: foundedResults,
-							});
-						} else {
-							const surveyUser = repository.create({
-								user_id: user.id,
-								survey_id: survey.id,
-							});
-
-							const newSurveyUser = await repository.save(
-								surveyUser
-							);
-
-							await SendMailService.execute(
-								email,
-								survey.title,
-								variables,
-								emailPath
-							);
-
-							return res.status(201).json({
-								message: 'Survey email has send with success!',
-								code: 201,
-								info: newSurveyUser,
-							});
-						}
-					})
-					.catch((error) => {
-						// Survey not found
-						return res.status(400).json({
-							code: 400,
-							message: `Survey with id ${survey_id} not found!`,
-						});
-					});
-			})
-			.catch((error) => {
-				// User not found
-				return res.status(404).json({
-					code: 404,
-					message: 'User not found!',
+				const foundedResults = await repository.findOne({
+					where: [{ user_id: user.id }, { value: null }],
+					relations: ['user', 'survey'],
 				});
+
+				if (foundedResults) {
+					await SendMailService.execute(
+						email,
+						survey.title,
+						variables,
+						emailPath
+					);
+
+					return res.status(200).json({
+						message: 'Survey email has send with success',
+						code: 200,
+						info: foundedResults,
+					});
+				} else {
+					const surveyUser = repository.create({
+						user_id: user.id,
+						survey_id: survey.id,
+					});
+
+					const newSurveyUser = await repository.save(surveyUser);
+
+					await SendMailService.execute(
+						email,
+						survey.title,
+						variables,
+						emailPath
+					);
+
+					return res.status(201).json({
+						message: 'Survey email has send with success!',
+						code: 201,
+						info: newSurveyUser,
+					});
+				}
+			} else {
+				// Survey not found
+				return res.status(400).json({
+					code: 400,
+					message: `Survey with id ${survey_id} not found!`,
+				});
+			}
+		} else {
+			// User not found
+			return res.status(404).json({
+				code: 404,
+				message: 'User not found!',
 			});
+		}
 	}
 }
 
